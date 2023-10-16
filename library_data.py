389c19f2e7988d1797acb1a02a9f53590cab4216
Sergei Krumas <cjkrumas@gmail.com>
2023-10-16 09:18:49 +0300

    small fix

--- library_data.py
+++ library_data.py
@@ -104,11 +104,11 @@ class LibraryData(BoardLibraryDataMixin, CommentingLibraryDataMixin, TaggingLibr
         dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
         for drive in drives:
-          files_count = 0
-          for cur_folder, dirs, files in os.walk(drive):
-            files_count += len(files)
-            print(cur_folder, len(files))
-          print(drive, files_count)
+            files_count = 0
+            for cur_folder, dirs, files in os.walk(drive):
+                files_count += len(files)
+                print(cur_folder, len(files))
+            print(drive, files_count)
 
     @staticmethod
     def get_content_path(folder_data):
