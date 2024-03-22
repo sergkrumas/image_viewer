7620194369678360debeb8770798d314aa5d114f
sergkrumas <cjkrumas@gmail.com>
2024-03-22 17:57:35 +0300

    refactoring

--- board.py
+++ board.py
@@ -2114,9 +2114,8 @@ class BoardMixin():
 
             elif ctrl and not shift:
                 cf = self.LibraryData().current_folder()
-                delta = QPointF(event.pos() - self.board_origin)
-                delta = QPointF(delta.x()/self.board_scale_x, delta.y()/self.board_scale_y)
-                cf.board.board_user_points.append([delta, self.board_scale_x, self.board_scale_y])
+                canvas_pos = self.board_map_to_board(event.pos())
+                cf.board.board_user_points.append([canvas_pos, self.board_scale_x, self.board_scale_y])
 
         elif event.button() == Qt.MiddleButton:
             if no_mod:
