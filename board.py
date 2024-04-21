08acbbab2d5a7c1cc25d353c54f4807651b49d76
sergkrumas <cjkrumas@gmail.com>
2024-04-21 19:06:37 +0300

    cleaning

--- board.py
+++ board.py
@@ -783,7 +783,7 @@ class BoardMixin(BoardTextEditItemMixin):
 
         self.LibraryData().make_viewer_thumbnails_and_library_previews(fd, None)
         fd.board.ready = True
-        self.LibraryData().load_board_data()
+        self.LibraryData().load_board_data() #callbacks are set here
         self.init_selection_bounding_box_widget(fd)
         self.build_board_bounding_rect(fd)
 
