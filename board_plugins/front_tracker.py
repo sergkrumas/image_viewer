b3a6f8b79ed7d65d30c792a97c1d428a43343768
sergkrumas <cjkrumas@gmail.com>
2024-04-22 12:42:42 +0300

    Boards: Front Tracker

--- board_plugins/front_tracker.py
+++ board_plugins/front_tracker.py
@@ -6,10 +6,18 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+def paintEvent(self, painter, event):
+    self.board_draw_main_default(painter, event)
 
+    painter.save()
 
+    offset = QPointF(300, 300)
+    a = self.board_MapToViewport(offset)
+    b = self.board_MapToViewport(offset*10)
+    rect = QRectF(a, b)
+    painter.fillRect(rect, QColor(20, 20, 20, 220))
 
-
+    painter.restore()
 
 
 
@@ -25,7 +33,7 @@ def register(board_obj, plugin_info):
     plugin_info.name = 'FRONT TRACKER'
     plugin_info.preparePluginBoard = preparePluginBoard
 
-
+    plugin_info.paintEvent = paintEvent
 
 if __name__ == '__main__':
     subprocess.Popen([sys.executable, "-u", "./../_viewer.pyw", "-board", os.path.basename(__file__)])
