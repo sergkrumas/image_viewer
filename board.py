1a9e8d87025a3262a122d286993a2d4a1569ba28
sergkrumas <cjkrumas@gmail.com>
2024-04-27 19:18:47 +0300

    refactoring item_scale_y

--- board.py
+++ board.py
@@ -80,17 +80,17 @@ class BoardItem():
         self.animated = False
 
         self.scale_x = 1.0
-        self.item_scale_y = 1.0
+        self.scale_y = 1.0
         self.position = QPointF()
         self.rotation = 0
 
         self.__scale_x = None
-        self.__item_scale_y = None
+        self.__scale_y = None
         self.__position = None
         self.__rotation = None
 
         self.__scale_x_init = None
-        self.__item_scale_y_init = None
+        self.__scale_y_init = None
         self.__position_init = None
 
         self.board_index = 0
@@ -178,19 +178,19 @@ class BoardItem():
         if scaled:
             if self.type == self.types.ITEM_IMAGE:
                 scale_x = self.scale_x
-                scale_y = self.item_scale_y
+                scale_y = self.scale_y
             elif self.type == self.types.ITEM_FOLDER:
                 scale_x = self.scale_x
-                scale_y = self.item_scale_y
+                scale_y = self.scale_y
             elif self.type == self.types.ITEM_GROUP:
                 scale_x = self.scale_x
-                scale_y = self.item_scale_y
+                scale_y = self.scale_y
             elif self.type == self.types.ITEM_FRAME:
                 scale_x = self.scale_x
-                scale_y = self.item_scale_y
+                scale_y = self.scale_y
             elif self.type == self.types.ITEM_NOTE:
                 scale_x = self.scale_x
-                scale_y = self.item_scale_y
+                scale_y = self.scale_y
         else:
             scale_x = 1.0
             scale_y = 1.0
@@ -225,7 +225,7 @@ class BoardItem():
         global_scaling = QTransform()
         translation = QTransform()
         if apply_local_scale:
-            local_scaling.scale(self.scale_x, self.item_scale_y)
+            local_scaling.scale(self.scale_x, self.scale_y)
         rotation.rotate(self.rotation)
         if apply_translation:
             if apply_global_scale:
@@ -264,18 +264,18 @@ class BoardItem():
                 self.item_width,
                 self.item_height,
                 self.scale_x,
-                self.item_scale_y
+                self.scale_y
             )
 
             self.local_end_point.setX(self.local_end_point.x() * self.scale_x)
-            self.local_end_point.setY(self.local_end_point.y() * self.item_scale_y)
+            self.local_end_point.setY(self.local_end_point.y() * self.scale_y)
 
             self.local_start_point.setX(self.local_start_point.x() * self.scale_x)
-            self.local_start_point.setY(self.local_start_point.y() * self.item_scale_y)
+            self.local_start_point.setY(self.local_start_point.y() * self.scale_y)
 
             self.item_width *= self.scale_x
-            self.item_height *= self.item_scale_y
-            self.scale_x = self.item_scale_y = 1.0
+            self.item_height *= self.scale_y
+            self.scale_x = self.scale_y = 1.0
 
     def disable_distortion_fixer(self):
         if hasattr(self, '_saved_data'):
@@ -284,7 +284,7 @@ class BoardItem():
             self.item_width, \
             self.item_height, \
             self.scale_x, \
-            self.item_scale_y = self._saved_data
+            self.scale_y = self._saved_data
 
 BoardCallbacksNames = [
     'mousePressEvent',
@@ -1194,7 +1194,7 @@ class BoardMixin(BoardTextEditItemMixin):
                     fd.board.items_list.append(fd_bi)
                     fd_bi.board_index = i
                     fd_bi.scale_x = 1.0
-                    fd_bi.item_scale_y = 1.0
+                    fd_bi.scale_y = 1.0
 
                     fd_bi.position = offset + QPointF(pixmap.width(), pixmap.height())/2
                     offset += QPointF(pixmap.width(), 0)
@@ -2802,8 +2802,8 @@ class BoardMixin(BoardTextEditItemMixin):
             for bi in self.selected_items:
                 if bi.__scale_x is not None:
                     bi.scale_x = bi.__scale_x
-                if bi.__item_scale_y is not None:
-                    bi.item_scale_y = bi.__item_scale_y
+                if bi.__scale_y is not None:
+                    bi.scale_y = bi.__scale_y
                 if bi.__position is not None:
                     bi.position = bi.__position
 
@@ -2856,11 +2856,11 @@ class BoardMixin(BoardTextEditItemMixin):
 
         for bi in self.selected_items:
             bi.__scale_x = bi.scale_x
-            bi.__item_scale_y = bi.item_scale_y
+            bi.__scale_y = bi.scale_y
             bi.__position = QPointF(bi.position)
             if not viewport_zoom_changed:
                 bi.__scale_x_init = bi.scale_x
-                bi.__item_scale_y_init = bi.item_scale_y
+                bi.__scale_y_init = bi.scale_y
                 bi.__position_init = QPointF(bi.position)
             position_vec = bi.calculate_absolute_position(board=self) - self.scaling_pivot_corner_point
             bi.normalized_pos_x, bi.normalized_pos_y = self.calculate_vector_projection_factors(x_axis, y_axis, position_vec)
@@ -2934,9 +2934,9 @@ class BoardMixin(BoardTextEditItemMixin):
             x_factor, y_factor = self.calculate_vector_projection_factors(scaling_x_axis, scaling_y_axis, scaling_vector)
 
             bi.scale_x = bi.__scale_x * x_factor
-            bi.item_scale_y = bi.__item_scale_y * y_factor
+            bi.scale_y = bi.__scale_y * y_factor
             if proportional_scaling and not multi_item_mode and not center_is_pivot:
-                bi.scale_x = math.copysign(1.0, bi.scale_x)*abs(bi.item_scale_y)
+                bi.scale_x = math.copysign(1.0, bi.scale_x)*abs(bi.scale_y)
 
             # position component
             if center_is_pivot:
@@ -2964,7 +2964,7 @@ class BoardMixin(BoardTextEditItemMixin):
         if cancel:
             for bi in self.selected_items:
                 bi.scale_x = bi.__scale_x_init
-                bi.item_scale_y = bi.__item_scale_y_init
+                bi.scale_y = bi.__scale_y_init
                 bi.position = QPointF(bi.__position_init)
         else:
             self.init_selection_bounding_box_widget(cf)
