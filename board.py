04998daf1f6782f906a64c28ae2ba2a56f6cb6cf
sergkrumas <cjkrumas@gmail.com>
2024-05-10 12:01:24 +0300

    crash fixed

--- board.py
+++ board.py
@@ -941,7 +941,7 @@ class BoardMixin(BoardTextEditItemMixin):
 
         if isinstance(obj, self.BoardItem):
             if obj.type == self.BoardItem.types.ITEM_NOTE:
-                board_TextElementInitAfterLoadFromFile(obj)
+                self.board_TextElementInitAfterLoadFromFile(obj)
 
     def board_object_attributes_to_serial(self, obj, new_obj_base, exclude=None):
         attributes = list(obj.__dict__.items())
