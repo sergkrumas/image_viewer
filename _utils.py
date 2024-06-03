a43231bfb15bed887f3f508a1ce92ca3402392e1
sergkrumas <cjkrumas@gmail.com>
2024-06-03 20:05:00 +0300

    Slice Pipette Tool

--- _utils.py
+++ _utils.py
@@ -790,3 +790,31 @@ class APNGMovie():
         self._speed = speed
     def frameCount(self):
         return self._frameCount
+
+def bresenhamsLineAlgorithm(x0, y0, x1, y1):
+    pixels_coords = []
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    x, y = x0, y0
+    sx = -1 if x0 > x1 else 1
+    sy = -1 if y0 > y1 else 1
+    if dx > dy:
+        err = dx / 2.0
+        while x != x1:
+            pixels_coords.append(QPoint(x, y))
+            err -= dy
+            if err < 0:
+                y += sy
+                err += dx
+            x += sx
+    else:
+        err = dy / 2.0
+        while y != y1:
+            pixels_coords.append(QPoint(x, y))
+            err -= dx
+            if err < 0:
+                x += sx
+                err += dy
+            y += sy
+    pixels_coords.append(QPoint(x, y))
+    return pixels_coords
