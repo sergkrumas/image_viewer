4a115b958e0e4c7819f909a86fcba26e736d97b9
Sergei Krumas <cjkrumas@gmail.com>
2026-01-31 00:51:04 +0300

    TODO added

--- board.py
+++ board.py
@@ -1770,6 +1770,7 @@ class BoardMixin(BoardTextEditItemMixin):
 
     def boards_save_expo_to_app_settings(self):
         def callback():
+            # TODO: так ничего на самом деле не сохраняется, ибо надо вызывать set_setting_value! Это баг!
             self.SettingsWindow.store_to_disk()
             self.show_center_label('easeInExpo saved to settings file!')
 
